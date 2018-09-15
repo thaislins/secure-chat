767d9526db6978eb703e43d23df4e5cce5757ae8
Thais Lins <thaislin@gmail.com>
2018-09-15 18:01:22 -0300

    Add key input definition

--- server.py
+++ server.py
@@ -8,30 +8,44 @@ clients = {}
 addresses = {}
 bufsize = 1024
 server_address = ('', 5354)
-quit_msg = '{quit}'
+quit_msg, rc4_msg, sdes_msg = "{quit}", "{rc4}", "{s_des}"
 server = socket(AF_INET, SOCK_STREAM)
-type_cryptography = s_des
+type_crypt = None
+name_cryptography = ''
+key = ''
 
 def accept_connections():
     """Sets up handling for incoming clients."""
     while True:
         client, client_address = server.accept()
         print("%s:%s has connected." % client_address)
-        client.send(bytes(type_cryptography.encode("Type your name and press enter!"), "utf8"))
+        client.send(bytes('{' + name_cryptography + '}', "utf8"))
+        client.recv(3)
+        client.send(bytes('key' + key, "utf8"))
+        client.send(bytes(type_crypt.encode("Type your name and press enter!"), "utf8"))
         addresses[client] = client_address
         Thread(target=handle_client, args=(client,)).start()
 
 def handle_client(client):  # Takes client socket as argument.
-    name = type_cryptography.decode(client.recv(bufsize).decode("utf8"))
+    name = type_crypt.decode(client.recv(bufsize).decode("utf8"))
+    client.send(bytes(type_crypt.encode("This chat is encrypted with " + name_cryptography), "utf8"))
     welcome = 'Welcome %s! If you want to quit, type {quit} to exit.' % name
-    client.send(bytes(type_cryptography.encode(welcome), "utf8"))
+    client.send(bytes(type_crypt.encode(welcome), "utf8"))
     join_chat = "%s has joined the chat!" % name
     broadcast(join_chat)
     clients[client] = name
 
     while True:
         try:
-            msg = type_cryptography.decode(client.recv(bufsize).decode("utf8"))
+            msg_raw = client.recv(bufsize).decode("utf8")
+            if msg_raw in [rc4_msg, sdes_msg]:
+                client.send(b'ack')
+                crypt_type(msg_raw[1:-1])
+                broadcast(msg_raw)
+                broadcast("This chat is encrypted with " + name_cryptography)
+                continue
+
+            msg = type_crypt.decode(msg_raw)
             if msg == quit_msg:
                 remove_client(client, name)
                 break
@@ -40,6 +54,8 @@ def handle_client(client):  # Takes client socket as argument.
         except BrokenPipeError:
             remove_client(client,name)
             break
+        except ConnectionResetError:
+            break
 
 def remove_client(client, name):
     del clients[client]
@@ -47,22 +63,31 @@ def remove_client(client, name):
     print("%s:%s has disconnected." % addresses[client])
     client.close()
 
-def broadcast(msg, prefix=""):  # prefix is for name identification.
+def broadcast(msg, name=""):  
     """Broadcasts a message to all the clients."""
+    message = msg if msg in [rc4_msg, sdes_msg] else type_crypt.encode(name + str(msg))
     for c in clients:
-        c.send(bytes(type_cryptography.encode(prefix + str(msg)), "utf8"))
+        c.send(bytes((message), "utf8"))
 
-def crypt_type(type):
-    global type_cryptography
-    type_cryptography = rc4 if sys.argv[1] == 'rc4' else s_des
+def modify_key(k):
+    global key 
+    key = k
+    type_crypt.define_key(key)
+
+def crypt_type(name):
+    global type_crypt
+    global name_cryptography
+    name_cryptography = name
+    type_crypt = rc4.RC4() if name == 'rc4' else s_des.SDES()
+    if key != '': type_crypt.define_key(key)
 
 def run():
-    if len(sys.argv) == 2 and (sys.argv[1] == 'rc4' or sys.argv[1] == 's_des'):
+    if len(sys.argv) == 3 and (sys.argv[1] == 'rc4' or sys.argv[1] == 's_des'):
         crypt_type(sys.argv[1])
-        print(type_cryptography)
+        modify_key(sys.argv[2])
         server.bind(server_address)
         print('starting up on%s port %s' % server_address)
-        server.listen(2)
+        server.listen(5)
         print("Waiting for connection...")
         accept_thread = Thread(target=accept_connections)
         accept_thread.start()
@@ -73,6 +98,6 @@ def run():
         print('server.py #algorithm_name')
         print('Options for algorithm_name include:')
         print('1. rc4      2. s_des')
-        
+
 if __name__ == "__main__":
     run()
